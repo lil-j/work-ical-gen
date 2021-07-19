8328fec008636e3236d43a15c335529d0022263c
lil-j <jake@krunchy.net>
2021-07-19 00:18:31 -0400

    Select name

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,72 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { useState } from "react";
-import { readTable, getNames } from "../scripts/ReadTable";
-import { Text, Textarea, Input, Stack, Center, Button } from "@chakra-ui/react";
+import {useState} from "react";
+import {readTable, getNames} from "../scripts/ReadTable";
+import {Text, Textarea, Input, Stack, Center, Button} from "@chakra-ui/react";
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuIcon,
+    MenuCommand,
+    MenuDivider,
+} from "@chakra-ui/react";
+import {ChevronDownIcon} from "@chakra-ui/icons";
 
 export default function Home() {
-  const [scheduleInput, setScheduleInput] = useState("");
-  const [rows, setRows] = useState("");
-  const [name, setName] = useState("");
-  const sendTable = () => {
-      if (!rows) setRows(8);
-      const splitInput = readTable(scheduleInput);
-      console.log(splitInput)
-      const names = getNames(splitInput);
-      // fetchTitles(splitInput, rows);
-  }
-  return (
-    <div>
-        <Center>
-            <Stack spacing={3}>
-                <Text fontSize="3xl">Insert Restaurant Schedule</Text>
-                {/*<Textarea value={scheduleInput} onChange={(e) => setScheduleInput(e.target.value)} placeholder="Paste full schedule"/>*/}
-                {/*<Input placeholder="Amount of rows (8 if left empty)" value={rows} onChange={(e) => setRows(e.target.value)}/>*/}
-                <div contentEditable dangerouslySetInnerHTML={{__html: scheduleInput}} onInput={(e) => setScheduleInput(e.target.innerHTML)}/>
-                <Button onClick={sendTable}>Submit</Button>
-            </Stack>
-        </Center>
-    </div>
-  )
+    const [scheduleInput, setScheduleInput] = useState("");
+    const [rows, setRows] = useState("");
+    const [name, setName] = useState("");
+    const [names, setNames] = useState();
+    const [selectedName, setSelectedName] = useState();
+    const sendTable = () => {
+        if (!rows) setRows(8);
+        const splitInput = readTable(scheduleInput);
+        console.log(splitInput)
+        const names = getNames(splitInput);
+        setNames(names);
+        // fetchTitles(splitInput, rows);
+    }
+    return (
+        <div>
+            <Center>
+                <Stack spacing={3}>
+                    {
+                        !names ? <><Text fontSize="3xl">Insert Restaurant Schedule</Text>
+                            <div contentEditable dangerouslySetInnerHTML={{__html: scheduleInput}}
+                                 onInput={(e) => setScheduleInput(e.target.innerHTML)}/>
+                            <Button onClick={sendTable}>Submit</Button></> :
+                            <>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <Stack spacing={3}>
+                                    <Menu>
+                                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                                            Choose Your Name
+                                        </MenuButton>
+                                        <MenuList>
+                                            {
+                                                names.map((thisname) => (
+                                                    <MenuItem onClick={() => setSelectedName(thisname)}>{thisname}</MenuItem>
+                                                ))
+                                            }
+                                        </MenuList>
+                                    </Menu>
+                                    <Text fontSize={"3xl"}>Selected Name: <strong>{selectedName}</strong></Text>
+                                    <Button>Generate</Button>
+                                </Stack>
+
+
+                            </>
+                    }
+
+                </Stack>
+            </Center>
+        </div>
+    )
 }
